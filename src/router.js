51e1261a7e564840bd4bbe7ab59537beea3f4270
lele <ye2000104@gmail.com>
2021-01-06 17:42:07 +0800

    提交登录功能

--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    // 重定向
+    {path:'/',redirect:'/login'},
+    {path:'/login',component:Login},
+    {path:'/home',component:Home}
   ]
 })
+
+//为路由对象，添加beforEach 导航守卫
+router.beforeEach((to,from,next)=>{
+  // to:将要访问的路劲
+  // from:从那个路径跳转来
+  // next 是一个函数，表示放行
+  // 如果用户访问的是登录页，直接放行
+  if(to.path === '/login') return next();
+  // 从sessionStorage 中获得的保存token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  //没有token 强制跳转到登录页
+  if(!tokenStr) return next('/login')
+  next()
+  
+})
+
+export default router;
